e2b26865562d0ba60459e5587987db05b179e6a8
masa takeuchi <mrx514@yahoo.co.jp>
2019-09-02 15:33:25 +0000

    complicated

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favposts, through: :favorites, source: :micropost
+
   
   def follow(other_user)
     unless self == other_user
@@ -30,5 +33,21 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  
+  def like(micropost)
+    favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+
+  def unlike(micropost)
+    favorite = favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def  favpost?(micropost)
+    self.favposts.include?(micropost)
+  end
+  
+  
   
 end
